cb6a0c07b508c26116615ca2f003780e61523010
Daniel Goldwater <dangoldwater@gmail.com>
2023-03-18 20:43:36 +0000

    witched to DaVinci

--- openai_access.py
+++ openai_access.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pathlib
 import openai
 
 openai.api_key = os.environ.get("OPEN_AI_API_KEY")
@@ -26,8 +28,18 @@ def get_whether_or_not_paper_is_clinical_study(abstract:str):
     return query_gpt_3_5_turbo(main_text=abstract, query_primer=query_primer)
 
 def get_clinical_info(abstract:str):
-    query_primer = "What follows is an abstract drawn from a paper published on PubMed. Determine whether the paper describes clinical intervention being used to treat the condition. If it does, return the name of the treatment, and how effective it was. If the abstract does not describe the effectiveness of a treatment, return '0'. "
+    query_primer = "What follows is an abstract drawn from a paper published on PubMed. Determine whether the paper describes clinical intervention being used to treat the condition. If it does, return the name of the treatment, and how effective it was. If the abstract does not describe the effectiveness of a treatment, return '0'. Do not pad your answer with context; be concise."
     return query_gpt_3_5_turbo(main_text=abstract, query_primer=query_primer)
+
+def get_clinical_info_2(abstract:str):
+    query_primer = "What follows is an abstract drawn from a paper published on PubMed. Based on the abstract, fill out the following table. If the abstract describes a study investigating the effectiveness of a treatment, return fill in and return the following table: \n  TREATMENT_NAME: $$$$ \n EFFECTIVENESS: $$$$ \n \n If the abstract does not describe a study investigating the effectiveness of a treatment, return '0', and nothing else. "
+    return query_model(query_primer + abstract)
+    # return query_gpt_3_5_turbo(main_text=abstract, query_primer=query_primer)
+
+def query_model(query):
+    query_args = get_model_from_config()
+    query_args.update({"prompt": query})
+    return openai.Completion.create(**query_args)
 
 def get_davinci(input_string: str):
     pre_prompt = "All houses have corners"
@@ -43,4 +55,9 @@ def get_davinci(input_string: str):
     return response_davinci
 
 def get_message_from_response(response):
-    return response['choices'][0]['message']['content']
+    return response['choices'][0]['text']
+
+def get_model_from_config():
+    with open(pathlib.Path('config.json'), 'r') as file:
+        config = json.load(file)
+    return config
--- scratch.py
+++ scratch.py
@@ -3,22 +3,13 @@ import pandas
 import openai_access
 from scrape import Scrape_PubMed, scraper
 
-# with open('natural_language.txt', 'r') as file:
-#     textt = file.read()
-
-# # print(textt)
-# summ = openai_access.get_summary(textt)
-# print(summ['choices'][0]['content'])
 
 df = scraper.load_pubmed_data_from_query('eczema')
-
-# for ab in df['AB']:
-#     print(ab)
 
 abstracts = [a for a in df['AB'][0:10]]
 abstracts = [ a for a in abstracts if a]
 
 for ab in abstracts:
-    is_clinical = openai_access.get_clinical_info(abstract=ab)
+    is_clinical = openai_access.get_clinical_info_2(abstract=ab)
     is_clinical = openai_access.get_message_from_response(is_clinical)
     print(is_clinical)
